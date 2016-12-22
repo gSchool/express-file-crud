de256ee3cac71c15ba384bc789ba7373d106271a
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2016-12-22 14:40:07 -0700

    Implemented function and  GET /api/books.

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const dataStore = require('./data_store');
 const app = express();
 const port = process.argv[3] || 8000;
 
-app.listen(port, function() {
-  console.log("Listening on port: ",port);
+dataStore.load_from_file();
+dataStore.get_all_books();
+
+app.get('/api/books', (req, res) => {
+  let data = dataStore.get_all_books();
+  res.send(data);
 })
+
+app.listen(port, function() {
+    console.log("Listening on port: ",port);
+  })
--- data_store.js
+++ data_store.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const fs = require('fs');
+// const body_parser = require('body-parser');
+// const json_parser =body_parser.json();
 let global = null;
 
-module.exports = function load_from_file() {
-  fs.readFile(function(err, data) {
-    if (err) {
-      throw err;
-    }
-    global = data;
-  })
+module.exports = {
+  load_from_file () {
+    fs.readFile("./db/data.json", "utf-8", function(err, data) {
+      if (err) {
+        throw err;
+      }
+      global = data;
+    })
+  },
+  get_all_books() {
+    return global;
+  }
 }
