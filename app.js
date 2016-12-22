ed602082f9828fa1cd04a9a18f2caa4df3f842e6
Austin Edwards <austintedwards@gmail.com>
2016-12-22 12:07:29 -0700

    Part 6

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// const data = require('/data_store')
+const data = require('./data_store')
 const express = require('express');
 const app = express();
 const port = process.argv[2]||8000;
+
+data.load_from_file();
+
 
 app.listen(port, function(){
   console.log("Listen on port"+ port+". Go to http://localhost:"+port+"/");
--- data_store.js
+++ data_store.js
@@ -1,5 +1,11 @@
-// function load_from_file(){
-//   app.get('db/data.json',function(read){
-//
-//   });
-// }
+const fs = require('fs')
+var global = null;
+module.exports={
+ load_from_file(){
+  fs.readFile('db/data.json','utf8',(err,data)=>{
+    // if (err) throw err;
+     global = data;
+     console.log("books", global);
+  });
+  }
+}
