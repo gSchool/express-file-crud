e2243d0865090860ea91ca26554b26b1ea59252e
Tara Mason <tara.mason@gmail.com>
2016-12-22 11:21:41 -0700

    set up local server 8000

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function() {
+  console.log("example log listening on port 3000!");
+});
