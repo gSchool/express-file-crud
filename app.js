0545b686275a0415972ac24982e707ed5f691544
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2016-12-22 14:32:42 -0700

    implemented books by id

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
-const data_store = require('data_store')
-app.listen(2020, function(){
-  console.log('Listening on Port 2020. Go to http://localhost:2020');
+const ds = require('./data_store')
+const bp = require('body-parser');
+const jp = bp.json();
+let port = Number(process.argv[3]) || 8000;
+
+ds.load_from_file();
+
+app.listen(port, function(){
+  console.log(`Listening on Port ${port}. Go to http://localhost:${port}`);
+});
+app.get('/api', (req, res)=>{
+  res.send('How can we help you?');
+});
+app.get('/api/books', (req, res) => {
+  let data = ds.get_all_books();
+  let status = data ? 200 : 404;
+  res.status(status).send(data);
+});
+
+app.get('/api/books/:id', jp, (req, res) => {
+  let id = Number(req.params.id);
+  let data = ds.get_book_by_id(id);
+  let status = data ? 200 : 404;
+  res.status(status).send(data);
 });
--- data_store.js
+++ data_store.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 let global_data;
 module.exports = {
-  load_from_file(){
-    fs.readFile('./db/data.json', (err, data) => {
-      global_data = data;
-    })
-  }
+    load_from_file() {
+        fs.readFile('./db/data.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            global_data = data;
+        })
+    },
+    get_all_books() {
+        return global_data;
+    },
+    get_book_by_id(id) {
+        let books_object = JSON.parse(global_data);
+        let result = books_object.find(x => x.id === id);
+        return result ? result : undefined;
+    }
 };
