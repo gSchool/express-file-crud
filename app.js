0ee093d9f741e16f542c1b905b4b67b96b80a216
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-22 13:56:22 -0700

    Fixed seed/data.js to seed/data.json

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-// const http = require('http');
 const data_store = require('./data_store');
 
 app.use(function(req, res) {
   res.send('Bubble Butt, Bubble bubble bubble butt');
 })
 
+data_store.load_from_file()
 
 app.listen(4000, function() {
   console.log("Listening on port 4000. Go to http://localhost:4000/")
--- data_store.js
+++ data_store.js
@@ -1,1 +1,12 @@
+const fs = ('fs');
 
+module.exports = {
+  load_from_file: function() {
+    fs.readFile('./db/data', 'utf8', function(err, data) {
+      if(err) {
+        return err;
+      }
+      console.log(data);
+    })
+  }
+}
