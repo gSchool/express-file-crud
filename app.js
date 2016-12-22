9fcfc4f155f220a349fc50ee29a226ccbe3ea35b
jeffdiers <jeff.diers@gmail.com>
2016-12-22 11:35:36 -0700

    module is set up 💯

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const port = process.env.PORT || 8000
+const store = require('./data_store.js')
 
 app.listen(port, function(){
   console.log(`Listening for port ${port}. Go to http://localhost:8000/`);
