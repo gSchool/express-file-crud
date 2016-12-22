9f108a73c1f71a042781520a2e93ca5a3b896cd5
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2016-12-22 14:32:09 -0700

    Up to step 6

--- data_store.js
+++ data_store.js
@@ -1,1 +1,21 @@
-module.exports 
+const path = require('path')
+const fs = require('fs')
+let global = null;
+
+module.exports = {
+	load_from_file: function() {
+		fs.readFile( function(err, data) {
+
+		if(err){
+			throw err;
+		}
+		global = data;
+		
+		})
+	},
+	
+	get_all_books: function() {
+		return global;
+	}
+}
+
