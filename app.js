9bae645b0bfe7620bf01600ba42492f3272cb48d
JIV <JuanVerni@JIV-TB.local>
2016-12-22 14:05:06 -0700

    Step 4 Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+const express = require('express');
+//initialize
+const app = express();
+
+
+
+
+
+
+const port = 8000
+app.listen(port, function() {
+  console.log('Listening on port 8000');
+});
