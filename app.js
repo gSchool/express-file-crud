512f11d7c6895370f144e18a8fb3da4cfd777ab4
Anthony <anthony@Anthonys-MacBook-Air.local>
2016-12-22 11:51:41 -0700

    module created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const dataStore = require('./data_store')
 
 app.listen(port, function() {
     console.log("Go to http://localhost:8000");
