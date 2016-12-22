402f635df59710478dab6970047101ac5b97388e
bmatson <benjaminmatson@gmail.com>
2016-12-22 14:36:40 -0700

    implemented function to return all books and GET via /api/books

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const data_store = require('./data_store');
 
 data_store.load_from_file();
 
+
 app.listen(arg2, function() {
   console.log('Listening on port ' + arg2);
 });
 
-//
+app.get('/api/books', (req, res) => {
+  let books = data_store.get_all_books();
+  res.setHeader('Content-Type', 'application/json');
+  if (books === undefined) {
+    //return 404
+    res.status(404).send('Books not found.');
+  } else {
+    res.send(books);
+  }
+});
--- data_store.js
+++ data_store.js
@@ -1,5 +1,4 @@
-//module.exports = '';
-let loaded_data = {};
+let loaded_data = [];
 const fs = require('fs');
 
 module.exports.load_from_file = () => {
@@ -7,6 +6,10 @@ module.exports.load_from_file = () => {
   fs.readFile('./db/data.json', (err, data) => {
     if (err) throw err;
     loaded_data = data;
-    console.log('data loaded: ' + loaded_data);
   });
-}
+};
+
+module.exports.get_all_books = () => {
+  //returns an array of all books in memory
+  return loaded_data;
+};
