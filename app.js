169a5bc037984e7c84409d22e91be4a1c59b3a81
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2016-12-22 14:03:33 -0700

    added GET request on books

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const data_store = require('./data_store.js');
+const data_store = require('./data_store');
 
 data_store.load_from_file();
 
@@ -10,3 +10,9 @@ data_store.load_from_file();
 app.listen(port, function(){
   console.log("Listening on port 3000. Go to http://localhost:3000/");
 });
+
+// GET /api/books
+app.get('/api/books', (req, res) => {
+  let books = data_store.get_all_books();
+  res.send(books);
+})
--- data_store.js
+++ data_store.js
@@ -2,13 +2,20 @@
 
 const fs = require('fs');
 
+// Global data var
 let data_mem;
 
 module.exports = {
-  load_from_file: (data_mem) => {
+  load_from_file: () => {
     fs.readFile('db/data.json', 'utf8', (err, file_contents) => {
-      if (err) throw err;
+      if (err) {
+        console.log(err);
+      }
+      // assigning the file contents to my global var
       data_mem = file_contents;
     })
+  },
+  get_all_books: () => {
+    return data_mem;
   }
 }
