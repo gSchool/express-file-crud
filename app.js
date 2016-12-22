e540148c7e82e25a4e030f5cce8c7bc3b8c5b420
Austin Edwards <austintedwards@gmail.com>
2016-12-22 13:15:29 -0700

    Part 7

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const data = require('./data_store')
 const express = require('express');
 const app = express();
 const port = process.argv[2]||8000;
-
 data.load_from_file();
+
 
 
 app.listen(port, function(){
   console.log("Listen on port"+ port+". Go to http://localhost:"+port+"/");
 });
+
+let books = data.get_all_books();
+console.log ('the books', books)
--- data_store.js
+++ data_store.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
-var global = null;
+var globalBooks = null;
 module.exports={
  load_from_file(){
   fs.readFile('db/data.json','utf8',(err,data)=>{
-    // if (err) throw err;
-     global = data;
-     console.log("books", global);
+    if (err) throw err;
+     globalBooks = data;
   });
+},
+  get_all_books(){
+    return globalBooks
+    }
   }
-}
