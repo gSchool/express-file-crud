73a3cd1817b09247c065bbea4b26d53fb48c09b5
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2016-12-22 11:57:19 -0700

    reads db file into memory

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const data_store = require('./data_store.js');
+
+data_store.load_from_file();
+
 // listener
 app.listen(port, function(){
   console.log("Listening on port 3000. Go to http://localhost:3000/");
 });
-
-const data_store = require(./data_store.js);
--- data_store.js
+++ data_store.js
@@ -1,1 +1,14 @@
-module.exports = function ();
+'use strict'
+
+const fs = require('fs');
+
+let data_mem;
+
+module.exports = {
+  load_from_file: (data_mem) => {
+    fs.readFile('db/data.json', 'utf8', (err, file_contents) => {
+      if (err) throw err;
+      data_mem = file_contents;
+    })
+  }
+}
