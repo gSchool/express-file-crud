8995fd7f8be471af5339c9536cc4777086a4e9e6
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-22 12:11:38 -0700

    Add data-store module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+// const http = require('http');
+const data_store = require('./data_store');
 
 app.use(function(req, res) {
   res.send('Bubble Butt, Bubble bubble bubble butt');
