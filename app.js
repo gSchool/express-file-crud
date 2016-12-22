e3c7f69785e2c62cf9e7f4a46633aec3a91acf16
Austin Edwards <austintedwards@gmail.com>
2016-12-22 11:33:51 -0700

    Change port on part 3

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const data = require('/data_store')
+// const data = require('/data_store')
 const express = require('express');
 const app = express();
+const port = process.argv[2]||8000;
 
-app.listen(5050, function(){
-  console.log("Listen on port 5050. Go to http://localhost:5050/");
+app.listen(port, function(){
+  console.log("Listen on port"+ port+". Go to http://localhost:"+port+"/");
 });
---
+++ data_store.js
@@ -0,0 +1,5 @@
+// function load_from_file(){
+//   app.get('db/data.json',function(read){
+//
+//   });
+// }
