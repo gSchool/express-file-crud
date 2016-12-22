096a9ca857dae621d9e5be9654836b1b39db94f9
jeffdiers <jeff.diers@gmail.com>
2016-12-22 11:52:46 -0700

    read db into memory 👻

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const http = require('http')
 const port = process.env.PORT || 8000
 const store = require('./data_store.js')
 
+console.log(store());
+
 app.listen(port, function(){
   console.log(`Listening for port ${port}. Go to http://localhost:8000/`);
 })
--- data_store.js
+++ data_store.js
@@ -1,1 +1,10 @@
-module.exports
+const fs = require('fs')
+
+module.exports = function load_from_file() {
+  fs.readFile('./db/data.json', 'utf-8', (err, data) => {
+    if(err){
+      console.error("dumb dumb")
+    }
+    console.log(data)
+  })
+}
