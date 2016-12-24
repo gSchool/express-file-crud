9f5dddb203588c900903b924a635653c0ee7a1a9
jeffdiers <jeff.diers@gmail.com>
2016-12-24 13:31:15 -0700

    get book by id 📖

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const port = process.env.PORT || 8000
 const store = require('./data_store.js')
 
 store.load_from_file()
-store.get_all_books()
+// store.get_all_books()
+// store.get_book_by_id()
 
 app.listen(port, function(){
   console.log(`Listening for port ${port}. Go to http://localhost:8000/`);
+})
+
+app.get('/api/books', (req, res) => {
+  let books = store.get_all_books()
+  res.send(books)
+})
+
+app.get('/api/books/:id', (req, res) => {
+  let id = Number(req.params.id)
+  let book = store.get_book_by_id(id)
+  console.log(book);
+  res.send(book)
 })
--- data_store.js
+++ data_store.js
@@ -7,12 +7,24 @@ module.exports = {
     if(err){
       throw err
     }
-    obj = data
+    obj = JSON.parse(data)
     console.log(`this shit is being loaded ${data}`);
+    console.log(obj.length);
+
   })
 },
 
   get_all_books: () => {
-    console.log(obj);
+    return obj
+  },
+
+  get_book_by_id: (id) => {
+    for(i=0; i<obj.length; i++){
+    if(obj[i].id === id){
+      return obj[i]
+    }
   }
+  return undefined 
+},
+
 }
