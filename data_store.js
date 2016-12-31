77e83d0fd6aa5a7dbf37dfeb2b07d5a63b948dec
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2016-12-31 14:27:52 -0700

    Created a function to get book by ID and return the title

--- data_store.js
+++ data_store.js
@@ -4,72 +4,7 @@ const fs = require('fs');
 var glob_var;
 var book_list = [];
 
-<<<<<<< HEAD
-// function readContent(callback) {
-//     fs.readFile("./Index.html", function (err, content) {
-//         if (err) return callback(err)
-//         callback(null, content)
-//     })
-// }
-//
-// readContent(function (err, content) {
-//     console.log(content)
-// })
 
-
-// function to_be_called(callback) {
-//   fs.readFile('./db/seed.json', 'utf8' function read(err, data) {
-//     if(err) {
-//       throw err;
-//     } else {
-//       glob_var = data;
-//       console.log("This is inside function x")
-//       console.log(glob_var);
-//       callback();
-//     }
-//   })
-//
-// }
-//
-// console.log("This is BZs new way")
-// console.log(glob_var);
-
-
-// exports.load_from_file = to_be_called();
-
-module.exports = {
-  load_from_file: function() {
-    fs.readFile('./db/seed.json','utf8', function read(err, data) {
-      if (err) {
-        throw err;
-      } else {
-          glob_var = data;
-          console.log("This is inside the module.exports")
-          console.log(glob_var);
-          return glob_var;
-        }
-      })
-    }
-  get_all_books: function() {
-    return glob_var;
-  }
-
-
-  }
-
-
-
-
-// exports.get_all_books = get_all_books();
-//
-// function get_all_books() {
-//   for(i of data) {
-//     book_list.push(data[i].title)
-//
-//   }
-//   return book_list;
-// }
-=======
 
 
 module.exports = {
@@ -87,8 +22,17 @@ module.exports = {
 
   get_all_books: () => {
     return global_var;
+  },
+
+  get_book_by_id: function(id) {
+    for(i=0; i<global_var.length; i++) {
+      if (id = global_var[i].id) {
+        return global_var[i].title;
+      } else {
+        return undefined;
+      }
+    }
   }
 
 
 }
->>>>>>> get_function_branch
