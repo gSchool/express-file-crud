b4809d0d7ffd2970203cfe2f1ec83688373b1029
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2016-12-31 11:07:30 -0700

    Implemented a function that reads a file into memory.  Made multiple functions but couldn't test right.  Moving ahead.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 
+
 const express = require('express');
 const app = express();
+const data_store = require('./data_store')
 
 const port = 8000;
 
-const data_store = require('./data_store')
+
+
+
+let data = data_store.load_from_file;
+
+console.log("This is in app.js")
+console.log(data);
+
+
+
+
+
 
 app.listen(port, function () {
   console.log("Hello World")
-
 });
