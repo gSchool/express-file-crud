8c7910e62b1f87b8111bfbe46e1bbaa1394d85c4
Tara Mason <tara.mason@gmail.com>
2017-01-01 15:43:44 -0700

    completed added new book w unique id to data store

--- data_store.js
+++ data_store.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 let dataArray;
+let LAST_ID = 0;
 
 module.exports = {  //This is exporting a function (also could do module.exports.load_from_file or exports.load_from_file  )
 
@@ -11,6 +12,13 @@ module.exports = {  //This is exporting a function (also could do module.exports
         console.log(err);
       }
       dataArray = file_contents; // assigning file contents to global variable
+//make LAST_ID equal the largest id loaded from file
+//HOW DO I CHECK MY STEPS?????
+      for (var i=0; i < dataArray.length; i++) {
+        if (dataArray[i].id > LAST_ID) {
+          LAST_ID = dataArray[i].id;
+        }
+      }
     });
   },
 
@@ -27,15 +35,25 @@ module.exports = {  //This is exporting a function (also could do module.exports
          return "undefined";
   },
 
+//updates local file with new info
   write_to_file: (dataArray) => {  //fs.writeFile(filePath, data[, options], callback)
     fs.writeFile('.db/data.json', 'utf8', dataArray, (err) => {
       if (err) {
         throw err;
       } else {
-        console.log(`${file} written successfully`);
+        console.log('Written successfully');
       }
     });
+  },
+
+//write function called add book that takes obj as parameter
+  add_book: (newBook) => {
+    LAST_ID = LAST_ID + 1; //gives it unique id (just add 1 to LAST_ID)
+    dataArray.push(newBook); //adds it to books already in memory
+    write_to_file(); //calls write_to_file to update file
+    return newBook; //returns the new book (with its unique ID)
   }
+
 
 };
 
