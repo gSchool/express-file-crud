ef2435dfe4a3a48d439f12b4c9a7df91439b358f
Tara Mason <tara.mason@gmail.com>
2017-01-01 12:31:31 -0700

    9- implement finding book by id

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const storeMod = require('./data_store');  //This const (storeMod) is assigned to the module.exports object from data_store.js file//Need . on the require here
+const storeMod = require('./data_store');  //This const (storeMod) is assigned to the module.exports object from data_store.js file   // Need . on the require here
 //think of "require" as meaning 'require the module that is on this page (in the parenthesis).'
 storeMod.load_from_file();
 
@@ -15,8 +15,10 @@ app.listen(port, function() {
 
 // console.log("process.argv is:" + process.argv);
 
-app.get('/api/books', (req, res) => {
-  res.send(storeMod.get_all_books()); // to check: in console type: http GET localhost:3000/api/books
+// \/ \/ Routes HTTP GET requests to the specified path with the specified callback functions.
+var bookArray = app.get('/api/books', (req, res) => { //app is express: app.get(path, callbackFunction [,optionalCallback])
+  res.send(storeMod.get_all_books()); //res.send([body]) (body can be string, buffer object, or array.  Sends the http request
+  // to check: in console type: http GET localhost:3000/api/books
 });
 
 console.log(storeMod.get_all_books() ); // this doesn't work bc it needs to get the info
--- data_store.js
+++ data_store.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 let dataArray;
 
 module.exports = {  //This is exporting a function (also could do module.exports.load_from_file or exports.load_from_file  )
+
  load_from_file: () => {  //key is function name, rest is the function. This way you can call function by doing: something.function();
     fs.readFile('db/data.json', 'utf8', (err, file_contents) => {  // readFile(file,[options], callback) (callback = (err, data)) Reads contents of file. utf8 to make it readable (so don't have to stringify)
       if (err) {
@@ -12,9 +13,31 @@ module.exports = {  //This is exporting a function (also could do module.exports
       dataArray = file_contents; // assigning file contents to global variable
     });
   },
+
   get_all_books: () => {
     return dataArray;  //probably always need to return something, otherwise theres nothing to access in other file
+  },
+
+  get_book_by_id: function(id) {
+    for (var i = 0; i < dataArray.length; i++) {
+       if (dataArray[i].id === id) {
+         return dataArray[i].title;
+       }
+    }
+         return "undefined";
   }
+
 };
 
 console.log("working");
+
+// Copied from repl-check
+// function get_book_by_id(id) {
+//   for (var i = 0; i < dataArray.length; i++) {
+//      if (dataArray[i].id === id) {
+//        return dataArray[i].title;
+//      }
+//   }
+//        return "undefined";
+//
+//   }
