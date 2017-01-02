6f2aef226400bfeac361a1daa55e690088a15925
Anthony <anthonyguitarman13@gmail.com>
2017-01-02 15:59:04 -0700

    get all books

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var store = ""
  dataStore.load_from_file((data)=>{
    store = data
  })
- console.log(store);
-app.get("/",(req,res)=>{
-  res.send(store)
+
+ //console.log(store);
+app.get("/api/books",(req,res)=>{
+  res.send(dataStore.get_all_books(store))
 })
 
 
--- data_store.js
+++ data_store.js
@@ -2,10 +2,19 @@ const fs = require('fs')
 
 
 module.exports.load_from_file =
-     function(callback){
+    function(callback) {
+        fs.readFile('db/data.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            callback(JSON.parse(data))
+        })
+    }
 
-       fs.readFile('db/data.json', 'utf8', (err,data) => {
-        if (err) throw err;
-        callback(JSON.parse(data))
-      })
+module.exports.get_all_books =
+    function(store){
+      var titles = []
+      for (let i =0; i < store.length; i++){
+        titles.push(store[i].title)
+      }
+      console.log(titles);
+      return titles;
     }
