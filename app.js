55ad3e6a97a3581c1d452d06d53e95b92f0223f7
Anthony <anthonyguitarman13@gmail.com>
2017-01-02 11:43:41 -0700

    file load function

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 8000
 const dataStore = require('./data_store')
 
+const store =
+dataStore()
+// console.log(store);
+
+
+
 app.listen(port, function() {
     console.log("Go to http://localhost:8000");
 })
--- data_store.js
+++ data_store.js
@@ -1,3 +1,10 @@
-module.exports = function(){
-  
-}
+const fs = require('fs')
+
+module.exports =
+     function load_from_file(){
+      var store = fs.readFile('db/data.json', 'utf8', (err,data) => {
+        if (err) throw err;
+        store = data
+        console.log(store);
+      })
+    }
