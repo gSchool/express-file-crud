14d13158ec57468f8032645edfb829dce8a6d5b3
Austin Edwards <austintedwards@gmail.com>
2017-01-03 08:58:53 -0700

    Working on part 11

--- data_store.js
+++ data_store.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
-var globalBooks = null;
+let globalBooks = null;
+let LAST_ID = null;
 module.exports={
    load_from_file(){
       fs.readFile('db/data.json','utf8',(err,data)=>{
         if (err) throw err;
          globalBooks = data;
       });
+      booksID = JSON.parse(globalBooks)
+      LAST_ID = booksID.sort((a,b)=> a-b)[booksID.length-1].id;
+      console.log(LAST_ID)
     },
     get_all_books(){
       return globalBooks;
@@ -14,9 +18,15 @@ module.exports={
       let booksID = JSON.parse(globalBooks);
       let bookIf = booksID.find((item)=> item.id ===id)
       if (bookIf===""){
-        return undefined
+        return undefined;
       }else{
-        return bookIf
+        return bookIf;
       }
-      }
-  }
+      },
+    write_to_file(){
+      fs.writeFile('db/data.json',globalBooks,(err)=>{
+        if (err) throw err;
+        console.log("saved");
+      });
+    }
+  };
