eee1c711fb1e8aa85f9e16e9fd3d441c3963ac31
Anthony <anthonyguitarman13@gmail.com>
2017-01-04 08:23:32 -0700

    get books by id

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const dataStore = require('./data_store')
 var store = ""
  dataStore.load_from_file((data)=>{
    store = data
+   //dataStore.get_books_by_id(store,3)
  })
 
  //console.log(store);
 app.get("/api/books",(req,res)=>{
   res.send(dataStore.get_all_books(store))
+})
+
+app.get("/api/books/:id", (req, res) => {
+  let id = Number(req.params.id)
+  res.send(dataStore.get_books_by_id(store,id))
+  res.status(404)
 })
 
 
--- data_store.js
+++ data_store.js
@@ -1,20 +1,42 @@
 const fs = require('fs')
+let store = []
+let LAST_ID;
 
 
 module.exports.load_from_file =
     function(callback) {
         fs.readFile('db/data.json', 'utf8', (err, data) => {
             if (err) throw err;
-            callback(JSON.parse(data))
+            store = data
+
         })
     }
 
 module.exports.get_all_books =
-    function(store){
-      var titles = []
-      for (let i =0; i < store.length; i++){
-        titles.push(store[i].title)
-      }
-      console.log(titles);
-      return titles;
+    function(store) {
+        var titles = []
+        for (let i = 0; i < store.length; i++) {
+            titles.push(store[i].title)
+        }
+        //console.log(titles);
+        return titles;
     }
+
+module.exports.get_books_by_id =
+    function(store, id) {
+        for (let i = 0; i < store.length; i++) {
+            //console.log(store[i]);
+            if (store[i].id === id) {
+                //console.log(store[i].title);
+                return store[i].title;
+            }
+        }
+    }
+
+function write_to_file(obj) {
+    fs.writeFile('/db/data.json',obj,(err) => {
+        if (err) {
+            throw err
+        }
+    })
+}
