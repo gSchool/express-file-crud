0b9e561efb0670351b0bb4a369a1de79ebc646b8
JIV <JuanVerni@JIV-TB.local>
2017-01-04 09:18:10 -0700

    9AM commit

--- app.js
+++ app.js
@@ -4,12 +4,9 @@
 const express = require('express');
 const app = express();
 const data_store = require('./data_store');
- const port = process.env.PORT || 8000
-//step 6
-const loadFile = function load_from_file () {
-	
-}
+const port = process.env.PORT || 8000
 
+var readFile = load_from_file;
 
 
 
--- data_store.js
+++ data_store.js
@@ -1,3 +1,29 @@
-module.exports = {
+'use strict'
+
+var fs = require('fs'); //do I have to have this here???
+var path = require('path'); 
+var dataPath = path.join(__dirname, 'data.json');
+ 
+
+//step 6
+function load_from_file() {
+	fs.readFile(dataPath, 'utf8', function(err, dataJSON){
+		if (err) {
+			console.error(err.stack);
+			return res.sendStatus(500);
+		}
+
+		var myData = JSON.parse(dataJSON);
+		res.send(myData);
+	});
+} 
+
+//step 7
+function get_all_books() {
 	
+}
+
+
+module.exports = {
+	load_from_file: load_from_file;
 }
