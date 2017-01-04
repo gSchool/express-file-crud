f603f7198f35a0c54129ef9f51ee9fae449948ca
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2017-01-04 09:18:10 -0700

    Wednesday commit

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const app = express();
-const port = process.argv[3] || 8000;
-const data = require('./data_store.js')
-
-app.listen(port, function(){
-	console.log("Listening on port: ", port)
-});
+const express = require('express')
+const app = express()
+const port = 8000
+const dataStore = require('./data_store')
 
 
+dataStore.load_from_file();
+
+
+app.get("/api/books",(req,res)=>{
+	let all_books = dataStore.get_all_books();
+	let code = all_books ? 200 : 400;
+ res.status(code).send(all_books);
+})
+
+app.get("/api/books/:id", (req, res) => {
+ let id = Number(req.params.id)
+ let book = dataStore.get_books_by_id(id);
+ let code = book ? 200 : 400;
+ res.status(code).send(book)
+ 
+})
+
+
+
+
+app.listen(port, function() {
+   console.log("Go to http://localhost:8000");
+})
--- data_store.js
+++ data_store.js
@@ -1,21 +1,42 @@
-const path = require('path')
 const fs = require('fs')
-let global = null;
+let store;
+let LAST_ID;
+
 
 module.exports = {
-	load_from_file: function() {
-		fs.readFile( function(err, data) {
 
-		if(err){
-			throw err;
-		}
-		global = data;
-		
-		})
-	},
-	
-	get_all_books: function() {
-		return global;
-	}
+load_from_file(){
+
+    
+        fs.readFile('db/data.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            
+            store = JSON.parse(data)
+        })
+    },
+
+get_all_books(){
+   return store;
+
+    },
+
+get_books_by_id(id) = {
+
+      for(let i = 0; i< store.length; i++){
+        
+        if(store[i].id === id){
+        
+          return store[i].title;
+        }
+      }
+    }
+  }
+ function write_to_file(){
+  fs.writeFile()('db/data.json',JSON.stringify(store),'utf8', (err) => {
+    if (err){
+      throw err
+    }
+
+   
+ })
 }
-
